f8cfcf585883ca592d251182ca3452b553f6a4c4
1133859 <1133859@RICL-AI711304.us.ray.com>
2018-06-12 13:49:48 -0500

    Modified source code to be more testable. Turned Scanner in to a parameter

--- Udemy_PracticeJavaByBuildingProjects_StudentDataApp/src/student_database_app/Student.java
+++ Udemy_PracticeJavaByBuildingProjects_StudentDataApp/src/student_database_app/Student.java
@@ -5,12 +5,16 @@
  *  Works Cited:
  *  Course title = "Practice Java by Building Projects"
  *  Instructor = Tim Short
- *  https://www.udemy.com/practice-java-by-building-projects/learn/v4/t/lecture/8098812?start=0*/
+ *  https://www.udemy.com/practice-java-by-building-projects/learn/v4/t/lecture/8098812?start=0
+ *  https://stackoverflow.com/questions/6415728/junit-testing-with-simulated-user-input?utm_medium=organic&utm_source=google_rich_qa&utm_campaign=google_rich_qa*/
 
 package student_database_app;
 import static java.lang.System.out;
 
+import java.util.ArrayList;
 import java.util.InputMismatchException;
+import java.util.List;
+import java.util.NoSuchElementException;
 import java.util.Scanner;
 
 public class Student {
@@ -18,28 +22,38 @@ public class Student {
 	private String lastName;
 	private String gradeYear;
 	private Integer studentID;
-	private String courses;
+	private List<String> coursesList;
 	private Integer tuitionBalance;
 	private Integer costOfCourse;
+	private Scanner classScanner;
 	
 	// Constructor prompt for student name.
-	public Student() {
-		Scanner userInput = new Scanner(System.in);
-		Integer temp = -1;
+	public Student(Scanner scan) {
+		this.classScanner = scan;
+		this.coursesList = new ArrayList<String>();
+		this.tuitionBalance = 0;
+		this.costOfCourse = 600;
+		
+		this.classScanner = this.setName(this.classScanner);
+		
+		this.classScanner = gradChoice(this.classScanner);
+		
+		
+	}
+	private Scanner setName(Scanner scan) {
+		//Scanner userInput = new Scanner(System.in);
 		
 		out.printf("%s%n","Please enter your first name: ");//"%n" is better than "\n" because it lets Java change the new line encoding based on the system it is running on.
-		this.firstName = userInput.nextLine();
+		this.firstName = scan.nextLine();
 		
 		out.printf("%s%n","Please enter your last name: ");
-		this.lastName = userInput.nextLine();
+		this.lastName = scan.nextLine();
 		
-		this.gradeYear = gradChoice();
-		
-		userInput.close();
+		return scan;
 	}
-	private String gradChoice() {
-		Scanner userInput = new Scanner(System.in);
-		Integer choice;
+	private Scanner gradChoice(Scanner scan) {
+		//Scanner userInput = new Scanner(System.in);
+		Integer choice = -1;
 		String theChoice;
 		
 		out.printf("%s%n%s%n%s%n%s%n%s%n", 
@@ -52,13 +66,16 @@ public class Student {
 		
 		try
 		{
-			choice = userInput.nextInt();
+			choice = (Integer)(scan.nextInt());
+
 		}
-		catch(InputMismatchException e)
+		catch(NoSuchElementException e)
 		{
+			out.println("***************");
+			out.println(choice);
+			out.println("***************");
 			choice = 1;
 		}
-		 userInput.close();
 		
 		switch(choice) {
 			case 1: theChoice = "Freshman";
@@ -72,11 +89,63 @@ public class Student {
 			default: theChoice =   "none";
 					break;
 		}
-		return theChoice;//it is good practice to not have multiple return statements in a single function
+		this.gradeYear = theChoice;
+		return scan;//it is good practice to not have multiple return statements in a single function
+	}
+	//Enroll
+	public Scanner enroll(Scanner scan) {
+		String course = "Q";
+		//Scanner userInput = new Scanner(System.in);
+		do {
+			
+			out.printf("%s%n", "Enter course to enroll (Q to quit): ");
+			//userInput.nextLine();
+			course = scan.nextLine();
+			if(!(course.equals("Q") || course.equals("q"))) {
+			this.coursesList.add(course);
+			this.tuitionBalance = this.tuitionBalance + this.costOfCourse;
+			}
+			
+		} while(!(course.equals("Q") || course.equals("q")));
+		
+		return scan;
+
+	}
+	//View balance
+	public Integer getBalance() {
+		return this.tuitionBalance;
+	}
+	//Pay Tuition
+	public Scanner payTuition(Scanner scan) {
+		out.printf("%n%s%n", "Please enter your payment: ");
+		//Scanner in = new Scanner(System.in);
+		Integer payment = scan.nextInt();
+		this.tuitionBalance = this.tuitionBalance - payment;
+		out.printf("%s %d%n", "Thank you for your payment of:", payment);
+		out.printf("%s %d %s%n", "You have", this.getBalance(), "left.");
+		
+		return scan;
 	}
 	@Override
 	public String toString() {
-		return String.format(this.firstName + " " + this.lastName + " " + this.gradeYear);
+		String studentData;
+		studentData = String.format("%n" + this.firstName +" "+ this.lastName + " is a " + this.gradeYear + "."
+				+ "%nClasses enrolled in: " + this.coursesList
+				+ "%nYou now owe: " + this.tuitionBalance);
+		//studentData = studentData + String.format( "%n" + this.firstName + " " + this.lastName + " " + this.gradeYear);
+		return studentData;
+	}
+	public String getFirstName() {
+		return firstName;
+	}
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+	public String getLastName() {
+		return lastName;
+	}
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
 	}
 	
 	// Generate ID
--- Udemy_PracticeJavaByBuildingProjects_StudentDataApp/src/student_database_app/StudentDatabaseApp.java
+++ Udemy_PracticeJavaByBuildingProjects_StudentDataApp/src/student_database_app/StudentDatabaseApp.java
@@ -5,16 +5,41 @@
  *  Works Cited:
  *  Course title = "Practice Java by Building Projects"
  *  Instructor = Tim Short
- *  https://www.udemy.com/practice-java-by-building-projects/learn/v4/t/lecture/8098812?start=0*/
+ *  https://www.udemy.com/practice-java-by-building-projects/learn/v4/t/lecture/8098812?start=0
+ *  https://stackoverflow.com/questions/6415728/junit-testing-with-simulated-user-input?utm_medium=organic&utm_source=google_rich_qa&utm_campaign=google_rich_qa*/
 
 package student_database_app;
 
 import static java.lang.System.out;
 
+import java.io.ByteArrayInputStream;
+import java.util.Scanner;
+
+
 public class StudentDatabaseApp {
 
 	public static void main(String[] args) {
-		Student student_1 = new Student();
+		//String testString = String.format("%s%n%s%n%s", "Mouse", "Cars", "Extra");
+		//String testStringConstructor = String.format("%s%n%s%n%s%n%s%n%s%n%s%n%s", "James", "Hawley", "1", "history", "math", "q", "21");
+		String testStringConstructor = String.format("%s%n%s%n%s", "James", "Hawley", "2");
+		String testStringEnroll = String.format("%s%n%s%n%s", "history", "math", "q");
+		String testStringPay = String.format("%s", "21");
+		ByteArrayInputStream in_1 = new ByteArrayInputStream(testStringConstructor.getBytes());
+		ByteArrayInputStream in_2 = new ByteArrayInputStream(testStringEnroll.getBytes());
+		ByteArrayInputStream in_3 = new ByteArrayInputStream(testStringPay.getBytes());
+		Scanner scanUser = new Scanner(System.in);
+		Scanner scanStream_1 = new Scanner(in_1);
+		Scanner scanStream_2 = new Scanner(in_2);
+		Scanner scanStream_3 = new Scanner(in_3);
+		//out.println(scanStream.nextLine());
+		/*while (scan.hasNextLine()) {
+			out.println(scan.nextLine());
+			scan.next();
+		}*/
+		Student student_1 = new Student(scanStream_1);
+		student_1.enroll(scanStream_2);
+		//out.printf("You now owe: %d", student_1.getBalance());
+		student_1.payTuition(scanStream_3);
 		out.printf("%s%n",student_1.toString());
 	}
 }
